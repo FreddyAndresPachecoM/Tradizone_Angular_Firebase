1946ee668c389ba759d76752f0c4a1622a89f959
Eduardo Mendieta <chrisseduu13mc@gmail.com>
2021-04-24 16:21:02 -0500

    fromulario de restaurante terminado

--- src/app/components/restaurantes/registro-restaurautes/registro-restaurautes.component.ts
+++ src/app/components/restaurantes/registro-restaurautes/registro-restaurautes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/service/auth.service';
 import { AngularFireStorage } from '@angular/fire/storage';
 import { finalize } from 'rxjs/operators';
@@ -14,18 +14,17 @@ import { Router } from '@angular/router';
 })
 export class RegistroRestaurautesComponent implements OnInit {
 
-  restaurante: any = {};
+  private restaurante: any = {};
 
-  imagen : File = null;
-
-  //urlImagen: Observable<string>;
+  private imagen : File = null;
+  private imagenPath: string;
 
   formularioRegistro = new FormGroup({
-    nombre: new FormControl(''),
-    descripcion: new FormControl(''),
-    direccion: new FormControl(''),
-    telefono: new FormControl(''),
-    zona: new FormControl('')
+    nombre: new FormControl('', Validators.required),
+    descripcion: new FormControl('', Validators.required),
+    direccion: new FormControl('', Validators.required),
+    telefono: new FormControl('', Validators.required),
+    zona: new FormControl('', Validators.required)
   });
 
   constructor(private authService: AuthService, private storage: AngularFireStorage, 
@@ -38,45 +37,39 @@ export class RegistroRestaurautesComponent implements OnInit {
     this.imagen = e.target.files[0];
   }
 
-  async subirImagen(){
-      const id = Math.random().toString(36).substring(2);//id unico para esta imagen.
-      const filePath = `logos_restaurante/logo_${id}`;
-      const ref = this.storage.ref(filePath);
-      return this.storage.upload(filePath, this.imagen);
-      //const task = this.storage.upload(filePath, this.imagen);
-      //await task.snapshotChanges().pipe(finalize(() => this.urlImagen = ref.getDownloadURL())).subscribe();
+
+  registrarRestaurante(){
+    const {nombre, descripcion, direccion, telefono, zona} = this.formularioRegistro.value;
+    if(this.imagen != null && this.formularioRegistro.valid){
+      const usuarioLogueado = this.authService.getUsuarioLogeado();
+      usuarioLogueado.then(
+        data => {
+          this.restaurante['user_id'] = data.uid;
+          this.restaurante['restaurant_name'] = nombre;
+          this.restaurante['restaurant_description'] = descripcion;
+          this.restaurante['restaurant_location'] = direccion;
+          this.restaurante['restaurant_phone'] = telefono;
+          this.restaurante['restaurant_zone'] = zona;
+          this.crearRestaurante(this.restaurante, this.imagen);
+        },
+        err => alert('¡A ocurrido un problema al obtener el usuario autenticado!')
+      );
+    }else alert('¡Debe llenar todos los campos!');
   }
+  
 
-  async registrarRestaurante() {
-    this.subirImagen().then(
-      data => console.log(data),
-      err => console.log('error al subir la imagen')
-    );
-    
-    /*const {nombre, descripcion, direccion, telefono, zona, urlLogo} = this.formularioRegistro.value;
-    const usuarioLogueado = this.authService.getUsuarioLogeado();*/
-
-   /* usuarioLogueado.then(
-      data => {
-        this.restaurante['user_id'] = data.uid;
-      }
-    );
-
-    this.restaurante['restaurant_name'] = nombre;
-    this.restaurante['restaurant_description'] = descripcion;
-    this.restaurante['restaurant_location'] = direccion;
-    this.restaurante['restaurant_phone'] = telefono;
-    this.restaurante['restaurant_zone'] = zona;
-    this.subirImagen();
-    if(this.urlImagen){
-      this.restaurante['restaurant_image'] = this.urlImagen;
-      this.restauranteService.crearRestaurante(this.restaurante);
-      alert('¡Restaurante registrado con exito!');
-      this.router.navigate(['/home']);
-    }*/
-
+  private crearRestaurante(restaurante: any, imagen: File){
+    this.imagenPath = `logos_restaurantes/${imagen.name}`;
+    const fileRef = this.storage.ref(this.imagenPath);
+    const task = this.storage.upload(this.imagenPath, imagen);
+    task.snapshotChanges().pipe(finalize(() => {
+      fileRef.getDownloadURL().subscribe(urlImagen => {
+        restaurante['restaurant_image'] = urlImagen;
+        this.restauranteService.crearRestaurante(restaurante);
+        alert("¡Restaurante registrado correctamente!")
+        this.router.navigate(['/home']);
+      }, 
+      err => alert("ocurrio un error al intentar obtener la url de la imagen!"))
+    })).subscribe();
   }
-
-
-
 }
